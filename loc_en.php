013519e6852019a1832979606ce810be718e32f4
Gabbalo <Gabbalo@users.noreply.github.com>
2015-02-18 08:29:50 +0100

    Create loc_en.php

---
+++ loc_en.php
@@ -0,0 +1,47 @@
+<?php
+# Lokalisierung für de
+
+$text_site_title = "Trashmail for Uberspace";
+$text_header = "Trashmail";
+$text_subheader = "Administration Console";
+
+$text_set_name = "Define name";
+$text_mailbox = "Mailbox";
+$text_or_in_caps = "OR";
+$text_random_address = "Random adress";
+$text_replace_prior_field = "replace the prior field";
+$text_parameters = "Parameter";
+$text_recipient = "Destination address";
+$text_time_to_live = "Time to live";
+$text_one_hour = "One hour";
+$text_two_hours = "Two hours";
+$text_twelve_hours = "Twelve hours";
+$text_one_day = "One day";
+$text_two_days = "Two days";
+$text_three_days = "Three days";
+$text_one_week = "One week";
+$text_no_limit = "No limit";
+$text_comment = "Comment";
+$text_reset_button = "Reset";
+$text_submit_button = "Submit";
+$text_creation_date = "Creation date";
+$text_mailbox_restrictions = "max. ".$max_mailboxname_length." chars, a-z, A-Z, 0-9, underscore, hyphen and dot";
+
+$text_currently_available_addresses = "Currently available addresses";
+$text_delete = "Delete";
+
+$text_create_file = "Creating file";
+$text_file_creation_error = "Cannot create file";
+$text_box_created = "Mailbox created";
+$text_box_exists = "Mailbox exists";
+$text_invalid_boxname = "Given mailbox name is invalid";
+
+$text_db_entry_removed = "Database entry removed successfully";
+$text_db_entry_error = "Error removing database entry";
+$text_file_removed = "File removed successfully";
+$text_file_error = "Error removing file";
+$text_back = "Back to form";
+
+$email_subject = "Trashmail removed!";
+$email_message = "The following mailbox was removed successfully: ";
+?>
