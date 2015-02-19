390d1bbd7412caea02c06d6e2e54906072982bd6
Gabriel <Gabbalo@users.noreply.github.com>
2015-02-19 11:20:33 +0100

    Update loc_en.php

--- loc_en.php
+++ loc_en.php
@@ -2,8 +2,6 @@
 # Lokalisierung für de
 
 $text_site_title = "Trashmail for Uberspace";
-$text_header = "Trashmail";
-$text_subheader = "Administration Console";
 
 $text_set_name = "Define name";
 $text_mailbox = "Mailbox";
@@ -12,7 +10,8 @@ $text_random_address = "Random adress";
 $text_replace_prior_field = "replace the prior field";
 $text_parameters = "Parameter";
 $text_recipient = "Destination address";
-$text_time_to_live = "Time to live";
+$text_expiration = "Expires";
+$text_infinite = "never";
 $text_one_hour = "One hour";
 $text_two_hours = "Two hours";
 $text_twelve_hours = "Twelve hours";
@@ -26,6 +25,20 @@ $text_reset_button = "Reset";
 $text_submit_button = "Submit";
 $text_creation_date = "Creation date";
 $text_mailbox_restrictions = "max. ".$max_mailboxname_length." chars, a-z, A-Z, 0-9, underscore, hyphen and dot";
+
+$text_tooltip="For help just hover the left column";
+$text_tooltip_mailbox1="Maximum string-length";
+$text_tooltip_chars="chars";
+$text_tooltip_mailbox2="Permitted special chars";
+$text_tooltip_and="and";
+$text_tooltip_mailbox3="Special chars must not be positioned at the beginning or the end.";
+$text_tooltip_mailbox4="With a trailing '-default', an address becomes a CatchAll-address. 'default' may than be changed to other words when using the address.";
+$text_tooltip_random1="Generates a random email-address with the format";
+$text_tooltip_random2="Date-RandomString";
+$text_tooltip_random3="Input in field '".$text_mailbox."' will be ignored!";
+$text_tooltip_recipient="This is where the emails will be routed. You can change the selection in 'vars.php'.";
+$text_tooltip_ttl="After this period, the email-address will be erased. If you use ''No limit' the address will not be removed automatically at any time.";
+$text_tooltip_comment="Here you can put the site, where you used the address e.g..";
 
 $text_currently_available_addresses = "Currently available addresses";
 $text_delete = "Delete";
